a24804a49bcdebbb0d1f431aaa32943222120519
Hada, Jason <jason.hada@skanska.com>
2018-09-10 20:41:13 -0400

    working

--- app/src/main/java/com/example/inclass03/MainActivity.java
+++ app/src/main/java/com/example/inclass03/MainActivity.java
@@ -7,41 +7,41 @@ import android.view.View;
 import android.widget.Toast;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+    public class MainActivity extends AppCompatActivity {
 
-    static String USER_KEY = "USER";
+        static String USER_KEY = "USER";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-        findViewById(R.id.submit).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            findViewById(R.id.submit).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
 
-                Intent i = new Intent(MainActivity.this, DisplayActivity.class);
+                    Intent i = new Intent(MainActivity.this, DisplayActivity.class);
 
-                EditText name = findViewById(R.id.name);
-                EditText email = findViewById(R.id.email);
-                EditText address = findViewById(R.id.address);
-                EditText phone = findViewById(R.id.phone);
-                EditText mood = findViewById(R.id.mood);
+                    EditText name = findViewById(R.id.name);
+                    EditText email = findViewById(R.id.email);
+                    EditText address = findViewById(R.id.address);
+                    EditText phone = findViewById(R.id.phone);
+                    EditText mood = findViewById(R.id.mood);
 
-                if(name.getText().toString().isEmpty() || email.getText().toString().isEmpty() || address.getText().toString().isEmpty()
-                        || phone.getText().toString().isEmpty() || mood.getText().toString().isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Please fill out all fields.", Toast.LENGTH_SHORT).show();
-                    return;
+                    if(name.getText().toString().isEmpty() || email.getText().toString().isEmpty() || address.getText().toString().isEmpty()
+                            || phone.getText().toString().isEmpty() || mood.getText().toString().isEmpty()) {
+                        Toast.makeText(MainActivity.this, "Please fill out all fields.", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    User user = new User(name.getText().toString(), email.getText().toString(), phone.getText().toString(), address.getText().toString(), mood.getText().toString());
+                    i.putExtra(USER_KEY, user);
+
+                    startActivity(i);
                 }
-                
-                User user = new User(name.getText().toString(), email.getText().toString(), phone.getText().toString(), address.getText().toString(), mood.getText().toString());
-                i.putExtra(USER_KEY, user);
+            });
 
-                startActivity(i);
-            }
-        });
+        }
+
 
     }
-
-
-}
